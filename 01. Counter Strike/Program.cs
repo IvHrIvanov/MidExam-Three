a9a9f6ca3e5685fd57ca112080a2e2aa1bd5e266
IvanHrIvanov <vinko96@abv.bg>
2020-11-04 14:41:04 +0200

    Add project files.

---
+++ 01. Counter Strike/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _01._Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int energy = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int winBattle = 0;
+            bool flag = false;
+            while (input != "End of battle")
+            {
+                int distance = int.Parse(input);
+
+                if (energy < distance)
+                {
+                    
+                    Console.WriteLine($"Not enough energy! Game ends with {winBattle} won battles and {energy} energy");
+                    flag = true;
+                    break;
+
+                }
+
+                energy -= distance;
+
+                winBattle++;
+
+                if (winBattle % 3 == 0)
+                {
+                    energy += winBattle;
+                }
+                input = Console.ReadLine();
+
+                if (flag)
+                {
+                    break;
+                }
+
+
+            }
+            if (!flag)
+            {
+                Console.WriteLine($"Won battles: {winBattle}. Energy left: {energy}");
+            }
+        }
+    }
+}
